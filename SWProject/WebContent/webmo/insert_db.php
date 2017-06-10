1edcab0d5c7e8f0e91c64254ec1f4fd8685eed0f
Nuka <commontoday@naver.com>
2017-06-11 07:31:08 +0900

    php - mysql connect 변경, echo 표시안됌

--- SWProject/WebContent/webmo/insert_db.php
+++ SWProject/WebContent/webmo/insert_db.php
@@ -1,18 +1,19 @@
 <?php
-	date_default_timezone_set('Asia/Seoul');
+	date_default_timezone_set ( 'Asia/Seoul' );
 
-	//$url = "http://" . $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
-	$today = date("Y-m-d");
-	$con = mysqli_connect("localhost", "root", "1234", "swproject");
+	$today = date ( "Y-m-d" );
+	$db_host = "localhost";
+	$db_user = "root";
+	$db_passwd = "1234";
+	$db_name = "2swproject";
+	$conn = mysqli_connect ( $db_host, $db_user, $db_passwd, $db_name );
+
 	$sql = "Insert INTO memo (text, today, url)
-			VALUES ('$_POST[text]', '$today', '$_POST[url]')";
-	
-	if(mysqli_query($con, $sql)){
-		echo "저장되었습니다.";
-		//echo "<a href='http://localhost:8080/SWProject/Main.jsp'>홈페이지로 이동</a>";
-		//어떻게 해야하지
-	}else{
-		echo "Error:" . mysqli_error($con);
+	VALUES ('text', '$today', 'url')";
+
+	if (mysqli_query ( $conn, $sql )) {
+		echo 'Success!';
+	} else {
+		echo 'Error:' . mysqli_error ( $conn );
 	}
-	
 ?>
